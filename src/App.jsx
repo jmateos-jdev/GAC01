057046b8318aa4dc4d998fdbd32cd27c098f342f
jmateos-jdev <joaquin.mateos@jdev.com.ar>
2024-04-30 12:54:36 -0300

    change button

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Hack a Boss</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Contador es: {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
