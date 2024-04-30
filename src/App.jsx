401f616e67590a3432edc9b60bf4ac62fe673a5a
jmateos-jdev <joaquin.mateos@jdev.com.ar>
2024-04-30 15:11:20 -0300

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(10);
+  const [count, setCount] = useState(10)
 
   return (
     <>
@@ -21,15 +21,10 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           Contador es: {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p>Creado durante curso GAC01</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
